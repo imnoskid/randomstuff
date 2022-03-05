1362d8b5add8c2bcb411fe19d40e5def5cd7d186
skid <69488666+imnoskid@users.noreply.github.com>
2022-03-05 02:21:45 +0100

    Update jan_ui_backup.lua

--- jan_ui_backup.lua
+++ jan_ui_backup.lua
@@ -983,7 +983,6 @@ function library:CreateWindow(name, size, hidebutton)
                                     keybind:Set("None")
                                 end
                             else
-				print(tostring((input.KeyCode)):split(".")[3].." "..tostring(keybind.value).." xd")
                                 if keybind.value ~= "None" and tostring((input.KeyCode)):split(".")[3] == keybind.value then
                                     toggle:Set(not toggle.CheckedFrame.Visible)
                                 end
